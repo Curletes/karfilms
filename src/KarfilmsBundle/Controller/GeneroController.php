5f3ec843f99f198835ed8a4d6248367e49e27498
Carlos <carvegnie@gmail.com>
2019-06-03 23:30:08 +0200

    Iniciada la creación de apartado para mostrar la película según el género elegido.

--- src/KarfilmsBundle/Controller/GeneroController.php
+++ src/KarfilmsBundle/Controller/GeneroController.php
@@ -8,121 +8,125 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use KarfilmsBundle\Entity\Genero;
 use KarfilmsBundle\Form\GeneroType;
 
-class GeneroController extends Controller
-{
+class GeneroController extends Controller {
+
     private $session;
-    
-    public function __construct()
-    {
+
+    public function __construct() {
         $this->session = new Session();
     }
-    
-    public function indiceGeneroAction()
-    {
+
+    public function mostrarGeneroAction() {
         $em = $this->getDoctrine()->getEntityManager();
         $genero_repo = $em->getRepository("KarfilmsBundle:Genero");
         $generos = $genero_repo->findAll();
-        
-        return $this->render('@Karfilms/genero/indicegenero.html.twig', [
-            "generos" => $generos
+
+        return $this->render('@Karfilms/genero/mostrargenero.html.twig', [
+                    "generos" => $generos
         ]);
     }
-    
-    public function addGeneroAction(Request $request)
-    {
+
+    public function categoriaGeneroAction($nombre) {
+        $em = $this->getDoctrine()->getEntityManager();
+        $pelicula_repo = $em->getRepository('KarfilmsBundle:Pelicula');
+        $peliculas = $pelicula_repo->findAll();
+        $genero_repo = $em->getRepository('KarfilmsBundle:Genero');
+        $genero = $genero_repo->findOneBy(["nombre" => $nombre]);
+
+        return $this->render('@Karfilms/genero/categoriagenero.html.twig', [
+                    'peliculas' => $peliculas,
+                    'genero' => $genero
+        ]);
+    }
+
+    public function indiceGeneroAction() {
+        $em = $this->getDoctrine()->getEntityManager();
+        $genero_repo = $em->getRepository("KarfilmsBundle:Genero");
+        $generos = $genero_repo->findAll();
+
+        return $this->render('@Karfilms/genero/indicegenero.html.twig', [
+                    "generos" => $generos
+        ]);
+    }
+
+    public function addGeneroAction(Request $request) {
         $genero = new Genero();
         $form = $this->createForm(GeneroType::class, $genero);
-        
+
         $form->handleRequest($request);
-        
-        if($form->isSubmitted())
-        {
-            if($form->isValid())
-            {
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
                 $em = $this->getDoctrine()->getEntityManager();
                 $genero = new Genero();
                 $genero->setNombre($form->get("nombre")->getData());
-                
+
                 $em->persist($genero);
                 $flush = $em->flush();
-                
-                if($flush == null)
-                {
+
+                if ($flush == null) {
                     $status = "Género añadido correctamente.";
-                }
-                else
-                {
+                } else {
                     $status = "Error al añadir el género.";
                 }
-            }
-            else
-            {
+            } else {
                 $status = "El género no se ha añadido porque el formulario no es válido.";
             }
-            
+
             $this->session->getFlashBag()->add("status", $status);
             return $this->redirectToRoute("indice_genero");
         }
-        
+
         return $this->render('@Karfilms/genero/addgenero.html.twig', [
-            "form" => $form->createView()
+                    "form" => $form->createView()
         ]);
     }
-    
-    public function eliminarGeneroAction($id)
-    {
+
+    public function eliminarGeneroAction($id) {
         $em = $this->getDoctrine()->getEntityManager();
         $genero_repo = $em->getRepository("KarfilmsBundle:Genero");
         $genero = $genero_repo->find($id);
-        
-        if(count($genero->getGeneropelicula()) == 0) 
-        {
+
+        if (count($genero->getGeneropelicula()) == 0) {
             $em->remove($genero);
             $em->flush();
         }
-        
+
         return $this->redirectToRoute("indice_genero");
     }
-    
-    public function editarGeneroAction($id, Request $request)
-    {
+
+    public function editarGeneroAction($id, Request $request) {
         $em = $this->getDoctrine()->getEntityManager();
         $genero_repo = $em->getRepository("KarfilmsBundle:Genero");
         $genero = $genero_repo->find($id);
-        
+
         $form = $this->createForm(GeneroType::class, $genero);
-        
+
         $form->handleRequest($request);
-        
-        if($form->isSubmitted())
-        {
-            if($form->isValid())
-            {
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
                 $genero->setNombre($form->get("nombre")->getData());
-                
+
                 $em->persist($genero);
                 $flush = $em->flush();
-                
-                if($flush == null)
-                {
+
+                if ($flush == null) {
                     $status = "Género editado correctamente.";
-                }
-                else
-                {
+                } else {
                     $status = "Error al editar el género.";
                 }
-            }
-            else
-            {
+            } else {
                 $status = "El género no se ha editado porque el formulario no es válido.";
             }
-            
+
             $this->session->getFlashBag()->add("status", $status);
             return $this->redirectToRoute("indice_genero");
         }
-        
+
         return $this->render('@Karfilms/genero/editargenero.html.twig', [
-            "form" => $form->createView()
+                    "form" => $form->createView()
         ]);
     }
+
 }
